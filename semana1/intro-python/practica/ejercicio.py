c23a341373e940ccd9a220ccfad6adda791e968e
DrakthinK <pxdxnzy@hotmail.es>
2022-10-22 17:31:58 -0500

    ignorev2

--- semana1/intro-python/practica/ejercicio.py
+++
@@ -1,3 +0,0 @@
-#icionario={x:str(x) for x in range(10,501) if x % int(max(list(str(x))))==0}
-dicionario={x:int(max(list(str(x)))) for x in range(10,501) if x % int(max(list(str(x))))==0}
-print(dicionario)
