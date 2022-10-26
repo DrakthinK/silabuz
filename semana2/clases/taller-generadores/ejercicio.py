73ec1fcba12bd3b2d7680f0ee6d9af07c8a7a5c0
DrakthinK <pxdxnzy@hotmail.es>
2022-10-26 00:44:40 -0500

    taller-generadores

---
+++ semana2/clases/taller-generadores/ejercicio.py
@@ -0,0 +1,22 @@
+
+#problema1
+""" lista=[15,20,18]
+nombres=['Marcelo','jose',"Juan"]
+
+for i in range(0,len(lista)):
+  print(f"{nombres[i]}:{lista[i]}") """
+#problema2
+""" lista=['hola',True,5,6.04]
+for count,value in enumerate(lista,start=11):
+    print(f"{count}->{value}") """
+
+#problema3
+#problema4
+""" def funcion_generadora(colletion):
+  for i in colletion:
+    if i%2==0:
+      yield i*2
+    else:
+      yield i+1
+
+print(list(funcion_generadora([1,2,3,4,5]))) """
